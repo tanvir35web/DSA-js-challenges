41e9b8beb413db5c65c3b7ea2cc3d2b67ae82098
tanvir35web <islammdtanvirul@gmail.com>
2023-12-22 01:13:23 +0600

    the string with the first letter of each word capitalized done

--- 01-basic-challenges-1/06-title-case/title-case.js
+++ 01-basic-challenges-1/06-title-case/title-case.js
@@ -1,3 +1,15 @@
-function titleCase() {}
 
+function titleCase(str) {
+    let words = str.toLowerCase().split(' ');
+
+    for(i=0; i < words.length; i++) {
+        words[i] = words[i][0].toUpperCase() + words[i].slice(1);
+
+    }
+    return words.join(' ');
+
+}
 module.exports = titleCase;
+
+
+
