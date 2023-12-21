bfb2f2b0da89c03c89eb65a49ef480055afe1490
tanvir35web <islammdtanvirul@gmail.com>
2023-12-22 00:15:46 +0600

    till then no. 5 problem done

--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,12 +1,21 @@
 function countOccurrences(word, letter) {
     let counter = 0;
-    word.split("").map((item) => {
-      if (item === letter) {
-          counter += 1;
+    // word.split("").map((item) => {
+    //   if (item === letter) {
+    //       counter += 1;
+    //   }
+    // });
+
+    for (i = 0; i < word.length; i++) {
+      if (word[i] === letter) {
+        counter++;
       }
-    });
+    }
+
+
     return counter;
   }
+
 
 module.exports = countOccurrences;
 
--- 01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,3 +1,12 @@
-function findMaxNumber() {}
+function findMaxNumber(arr) {
+    let max = arr[0];
+
+    for(i=1; i < arr.length; i++){
+        if(arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    return max;
+}
 
 module.exports = findMaxNumber;
