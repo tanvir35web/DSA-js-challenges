2941bdc8d4c81405ecc7711288ef215eb5a60c61
tanvir35web <islammdtanvirul@gmail.com>
2023-12-22 21:58:23 +0600

    Revers string done

--- 01-basic-challenges-1/07-reverse-string/reverse-string.js
+++ 01-basic-challenges-1/07-reverse-string/reverse-string.js
@@ -1,3 +1,13 @@
-function reverseString() {}
+function reverseString(str) {
+
+    let reversed = '';
+    
+    for(let i = str.length - 1 ; i >= 0; i--) {
+        reversed += str[i];
+    }
+
+    return reversed;
+}
+
 
 module.exports = reverseString;
